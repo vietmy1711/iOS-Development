3aa173a021694e512ec94b354e636920c223ab91
mm <zzvietmyzz@gmail.com>
2020-03-16 20:20:49 +0700

    AppIcon, Storyboard

---
+++ I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by MM on 3/16/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

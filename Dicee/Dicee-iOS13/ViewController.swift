3c6c1c6dda6ffeb15543bfff045a57843c1e1c51
mm <zzvietmyzz@gmail.com>
2020-03-17 16:53:09 +0700

    Finished Dice Rolling App

---
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
+}
+

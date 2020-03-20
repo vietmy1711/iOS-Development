d6a3fa16be3c9a14c9816a4eaf2f4c4d1a1d09cf
mm <zzvietmyzz@gmail.com>
2020-03-20 18:25:21 +0700

    Finish EggTimer

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
     
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle)
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @objc func update() {
+        if secondsPassed < totalTime {
+            let percentage: Float = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = percentage
+            secondsPassed = secondsPassed + 1
+        }
+        else {
+            progressBar.progress = 1.0
+            titleLabel.text = "Done!"
+            timer.invalidate()
+        }
+        
     }
-    
-
+    let eggTimes = ["Soft": 300, "Medium":420, "Hard": 720]
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        titleLabel.text = "Patient!"
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
 }
